0ffec472b96ae653e8e8c94a0564688e2039f0d8
Evan Elias Young <evanyoung762@gmail.com>
2022-03-25 17:01:17 -0500

    🎨 move project metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -5,38 +5,12 @@
 
 @author    Evan Elias Young
 @date      2022-02-22
-@date      2022-02-22
+@date      2022-03-25
 @copyright Copyright 2022 Evan Elias Young. All rights reserved.
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 if __name__ == "__main__":
-    setup(
-        name="eye-py-decorators",
-        version="1.0.0",
-        description="Decorators for Humans",
-        author="Evan Elias Young",
-        author_email="evanyoung762@gmail.com",
-        url="https://github.com/evaneliasyoung/eye-py-decorators",
-        license="GNU GPLv3",
-        package_dir={"": "src"},
-        py_modules=["eye_decorators"],
-        keywords=["decorators", "generic", "utility"],
-        platforms=["All"],
-        python_requires=">=3.9",
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-            "Natural Language :: English",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Topic :: Utilities",
-        ],
-        zip_safe=False,
-        packages=find_packages(where="src"),
-    )
+    setup()
